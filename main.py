94f75465ee097d2173fa8435f8c08a6c9ff4ed61
SurbhiKaushal <surbhikaushal8@gmail.com>
2025-09-15 18:29:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+class_names = ['airplane','automobile','bird','cat','deer','dog','frog','horse','ship','truck']
+print("Training set shape:", x_train.shape)
+print("Test set shape:", x_test.shape)
+plt.figure(figsize=(10,5))
+for i in range(10):
+    plt.subplot(2,5,i+1)
+    plt.imshow(x_train[i])
+    plt.title(class_names[y_train[i][0]])
+    plt.axis('off')
+plt.show()
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+    MaxPooling2D(2,2),
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+    Flatten(),
+    Dense(64, activation='relu'),
+    Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+history = model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print("Test accuracy:", test_acc)
+plt.plot(history.history['accuracy'], label='Train Accuracy')
+plt.plot(history.history['val_accuracy'], label='Test Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
